9361b30fdaac905382224982faf173b9f1ceb0ad
Tomasz Zabawka <tomasz.zabawka@stepstone.com>
2021-01-25 13:53:41 +0100

    client-app initial

---
+++ client-app/pages/index.tsx
@@ -0,0 +1,39 @@
+export default function Home() {
+  const handleSubmit = (event: MouseEvent) => {
+    event.preventDefault();
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: 'john',
+        password: 'changeme',
+      }),
+    });
+  };
+
+  return (
+    <div>
+      Login!
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="uname">
+          <b>Username</b>
+        </label>
+        <input type="text" placeholder="Enter Username" name="uname" required />
+
+        <label htmlFor="psw">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
